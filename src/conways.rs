7b46f2a9bc8cd0843e04fd4391f81d4a01eadc17
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2024-11-15 14:40:26 -0300

    Change implementation from HashSet to Vec<Vec<>>
    Originally, the idea was to only store alive cells in a
    hashset. However, after further thinking, this would complicate
    calculating the cells that need to be "revived".

--- src/conways.rs
+++ src/conways.rs
@@ -1,17 +1,40 @@
-use std::collections::HashSet;
+#[derive(Debug, Clone)]
+enum State {
+    Alive,
+    Dead
+}
 
-type Position = (i32, i32);
+type Position = (u32, u32);
 
 struct Grid {
-    alive_cells: HashSet<Position>,
-    upper_left: Position,
-    lower_right: Position,
+    cells: Vec<Vec<State>>,
 }
 
 
 impl Grid {
-    pub fn new(lower_limit: i32) -> Grid {
-        Grid {  alive_cells: HashSet::new(), upper_left: (0, 0), lower_right: (lower_limit, lower_limit) }
+    pub fn new(size: usize) -> Grid {
+        Grid { cells: vec![vec![State::Dead; size]; size], } 
+    }
+
+    pub fn change_state(&mut self, position: Position, state: State) {
+        let x: usize = position.0 as usize;
+        let y: usize = position.1 as usize;
+
+        let outer = &mut self.cells[x];
+        let inner = &mut outer[y];
+
+        *inner = state;
+    }
+
+    pub fn update(&mut self) {
+    }
+
+    fn update_cells(&mut self) {
+        // let new_grid = HashSet::new();
+        let iter = self.cells.iter().flatten().enumerate();
+        let a: Vec<_>  = iter.map(|a| println!("Jamon {}: {:?}", a.0, a.1)).collect();
+
     }
 
 }
+
