1a27e4db20b4ff020fdd0ce255edd59f58d7c86b
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2024-11-19 15:36:06 -0300

    Add window configuration

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,53 @@
 use macroquad::prelude::*;
 
-const UPDATE_INTERVAL: f64 = 1.0;
+const UPDATE_INTERVAL: f64 = 0.3;
+
+const SPACE_FOR_TEXT: i32 = 200;
 
 mod conways;
 
 use conways::*;
 
+fn conf() -> Conf {
+    Conf {
+        window_title: "Conway's Game of Life".to_string(),
+        window_height: 600 + SPACE_FOR_TEXT,
+        window_width: 600,
+        fullscreen: false,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main(conf)]
 async fn main() {
-    let mut grid = Grid::new(25);
+    let mut keys: [KeyCode; 10] = [KeyCode::Space; 10];
+    let mut offset = 0;
 
-    grid.change_state((2, 1), State::Alive);
-    grid.change_state((2, 2), State::Alive);
-    grid.change_state((2, 3), State::Alive);
-    grid.change_state((1, 3), State::Alive);
-    grid.change_state((0, 2), State::Alive);
+    let mut grid = Grid::new(22);
 
+    // grid.change_state((2, 2), State::Alive);
+    // grid.change_state((2, 3), State::Alive);
+    // grid.change_state((1, 3), State::Alive);
+    // grid.change_state((0, 2), State::Alive);
+
+    
     let mut last_updated = get_time();
+
+    let cube_width  = 30.0;
+    let cube_height = 30.0;
 
     loop {
         clear_background(GRAY);
+        println!("{:?}", keys);
+
+        if let Some(key) = get_last_key_pressed() {
+	  keys[offset] = key;
+	  offset += 1;
+	  if offset == keys.len() {
+	      offset = 0;
+	  }
+        }
 
         for x in 0..grid.get_height() {
 	  for y in 0..grid.get_width() {
